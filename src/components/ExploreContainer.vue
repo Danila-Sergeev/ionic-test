e91ba23dd3b1bf025baa7874446e0cabeb5983ff
Danila_Sergeev <danila.sergeev@roboline.ru>
2025-01-14 16:15:12 +0300

    make btn, input and modal

--- src/components/ExploreContainer.vue
+++ src/components/ExploreContainer.vue
@@ -1,24 +1,59 @@
 <template>
   <div id="container">
-    <strong>{{ name }}</strong>
-    <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+    <ion-item class="box">
+      <ion-input
+        ref="input1"
+        class="custom"
+        v-model="input1Value"
+        placeholder="Введите текст"
+      ></ion-input>
+    </ion-item>
+    <ion-button @click="transferText">Перенести текст</ion-button>
+
+    <ion-modal :is-open="isOpen">
+      <ion-header>
+        <ion-toolbar>
+          <ion-title>Modal</ion-title>
+          <ion-buttons slot="end">
+            <ion-button @click="setOpen(false)">Close</ion-button>
+          </ion-buttons>
+        </ion-toolbar>
+      </ion-header>
+      <ion-content class="ion-padding">
+        <p>
+         {{ input1Value }}
+        </p>
+      </ion-content>
+    </ion-modal>
   </div>
 </template>
 
 <script setup lang="ts">
-defineProps({
-  name: String,
-});
+  import { ref } from "vue";
+  import { IonButton, IonInput, IonModal } from "@ionic/vue";
+
+  // Переменные для хранения значений
+  const input1Value = ref("");
+
+  const isOpen = ref(false);
+
+  const setOpen = (open: boolean) => (isOpen.value = open);
+
+  // Функция переноса текста
+  const transferText = () => {
+    setOpen(true)
+  }
 </script>
 
 <style scoped>
 #container {
-  text-align: center;
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh; /* Высота окна */
+  padding: 16px;
+  box-sizing: border-box;
 }
 
 #container strong {
@@ -36,4 +71,11 @@ defineProps({
 #container a {
   text-decoration: none;
 }
+ion-input.custom {
+
+}
+ion-item.box{
+  margin-bottom: 30px;
+}
+
 </style>
